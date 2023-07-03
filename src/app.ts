35cfadde09e783ac7fb5d251c71389804044f2f4
cherartem <artem138927@gmail.com>
2023-07-03 11:46:21 +0200

    Add not found handler and error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
+import compression from "compression";
+import helmet from "helmet";
+import { errorHandler, notFoundHandler } from "./middleware/errors";
 import "dotenv/config";
 
 const app: Application = express();
@@ -16,16 +19,23 @@ if (process.env.DB_CONNECTION) {
   });
 }
 
+app.use(compression());
+app.use(helmet());
+
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello");
+  res.status(200).json({
+    message: "Hello World!",
+  });
 });
+
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server is running on port ${process.env.PORT}`)
-);
+app.listen(port, () => console.log(`Server is running on port ${port}`));
