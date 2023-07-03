880860c10e6afd26d9428a16aae3570e8ee0c212
cherartem <artem138927@gmail.com>
2023-07-03 11:11:06 +0200

    Configure project setup: Remove dist folder, set up MongoDB connection, and configure Express middleware

--- dist/app.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import "dotenv/config";
-const app = express();
-app.get("/", (req, res) => {
-    res.send("Hello");
-});
-app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`));
-//# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,30 @@
 import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
 import "dotenv/config";
 
 const app: Application = express();
 
+if (process.env.DB_CONNECTION) {
+  mongoose.connect(process.env.DB_CONNECTION);
+
+  const db = mongoose.connection;
+  db.on("error", console.error.bind(console, "connection error: "));
+  db.once("open", () => {
+    console.log("Connected successfully");
+  });
+}
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello");
 });
+
+const port = process.env.PORT || 5000;
 
 app.listen(process.env.PORT, () =>
   console.log(`Server is running on port ${process.env.PORT}`)
