91a00e5d2734a1911717c15f19b2e64001ecef7d
cherartem <artem138927@gmail.com>
2023-07-03 15:58:58 +0200

    Implement sign-in functionality

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -5,6 +5,7 @@ import { hash, compare } from "bcryptjs";
 import passwordSchema from "../auth/passwordSchema";
 import he from "he";
 import { User } from "../models/user";
+import { createAccessToken, createRefreshToken } from "../auth/jwtTokens";
 
 export const signUp = [
   body("fullname", "This field is required").trim().notEmpty().escape(),
@@ -58,6 +59,64 @@ export const signUp = [
 
       res.status(200).json({
         message: "You have successfully signed up",
+      });
+    }
+  }),
+];
+
+export const signIn = [
+  body("username", "This field is required")
+    .trim()
+    .notEmpty()
+    .escape()
+    .custom(async (value) => {
+      const user = await User.findOne({ username: value }).exec();
+
+      if (!user) {
+        throw new Error("A user with this username does not exist");
+      } else {
+        return true;
+      }
+    }),
+  body("password", "This field is required")
+    .trim()
+    .notEmpty()
+    .escape()
+    .custom(async (value, { req }) => {
+      const user = await User.findOne({ username: req.body.username }).exec();
+      const match = await compare(value, user!.password);
+
+      if (!match) {
+        throw new Error("The entered password is incorrect");
+      } else {
+        return true;
+      }
+    }),
+
+  expressAsyncHandler(async (req: Request, res: Response) => {
+    const validationErrors = validationResult(req);
+
+    if (!validationErrors.isEmpty()) {
+      res.status(400).json({
+        errors: validationErrors.array(),
+        formData: {
+          username: he.decode(req.body.username),
+          password: he.decode(req.body.password),
+        },
+      });
+    } else {
+      const user = await User.findOne({ username: req.body.username });
+
+      const accessToken = createAccessToken(user!);
+      const refreshToken = createRefreshToken(user!);
+
+      res.cookie("jid", refreshToken, {
+        httpOnly: true,
+      });
+
+      res.status(200).json({
+        message: "You have successfully signed in",
+        accessToken,
       });
     }
   }),
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { signUp } from "../controllers/userController";
+import { signIn, signUp } from "../controllers/userController";
 import { checkUserRegistration } from "../middleware/checkUserRegistration";
 
 const router = express.Router();
 
 router.post("/", checkUserRegistration, signUp);
+router.post("/sign-in", signIn);
 
 export default router;
